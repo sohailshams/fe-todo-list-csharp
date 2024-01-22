25e42052bd12d5292e0352a255424a8dc53df398
Sohail Shams <sohail.shams@airelogic.com>
2024-01-22 22:55:26 +0000

    Added delete and edit buttons.

--- src/components/TodolistCard.jsx
+++ src/components/TodolistCard.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 function TodolistCard({ title, description }) {
     return (
         <div className='border border-[1px] border-black my-3 w-48 py-3 px-1 text-center'>
-            <h2 className='text-lg font-bold'>{title}</h2>
+
+            <div className='mb-3 flex justify-center gap-x-1'>
+                <button><i class="fa-solid fa-trash"></i></button>
+                <button className='pl-3'><i class="fa-solid fa-pencil"></i></button>
+                <h2 className='pl-1 text-lg font-bold mb-1'>{title}</h2>
+            </div>
             <p>{description}</p>
         </div>
     )
